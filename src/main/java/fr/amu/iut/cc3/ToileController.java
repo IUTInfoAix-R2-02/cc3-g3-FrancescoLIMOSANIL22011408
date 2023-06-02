5d41aaee6fe577c2b8a175b6f3141eb91f88e94a
l22011408 <francesco.limosani@etu.univ-amu.fr>
2023-06-02 16:37:00 +0200

    affichage des points

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -1,28 +1,15 @@
 package fr.amu.iut.cc3;
 
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class ToileController implements Initializable {
@@ -32,9 +19,38 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
+    public TextField sourceOfEvent;
+
+    public TextField comp1;
+    public TextField comp2;
+    public TextField comp3;
+    public TextField comp4;
+    public TextField comp5;
+    public TextField comp6;
+    @FXML
+    private Circle Circle1;
+    @FXML
+    private Circle Circle2;
+    @FXML
+    private Circle Circle3;
+    @FXML
+    private Circle Circle4;
+    @FXML
+    private Circle Circle5;
+    @FXML
+    private Circle Circle6;
+
+    @FXML
+    private Button trace;
+    @FXML
+    private Button vide;
+
+    private ListView<Circle> lesCercles;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        lesCercles = new ListView<Circle>();
+        lesCercles.getItems().addAll(Circle1, Circle2, Circle3, Circle4, Circle5, Circle6);
     }
 
     int getXRadarChart(double value, int axe ){
@@ -47,5 +63,16 @@ public class ToileController implements Initializable {
                 *  (value / noteMaximale));
     }
 
-
+    @FXML
+    public void HandlerActionOnField(ActionEvent event){
+        sourceOfEvent = (TextField) event.getSource();
+        int axe = GridPane.getRowIndex(sourceOfEvent);
+        double value = Integer.parseInt(sourceOfEvent.getText());
+        int centerX = getXRadarChart(value, axe);
+        int centerY = getYRadarChart(value, axe);
+        Circle leCircle = lesCercles.getItems().get(axe);
+        leCircle.setCenterX(centerX);
+        leCircle.setCenterY(centerY);
+        leCircle.setVisible(true);
+    }
 }
